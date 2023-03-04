fc6097f5290413ee0377e5a58794124ef4bf1202
Eric Mercer <egm@cs.byu.edu>
2023-03-04 14:04:09 +0000

    Add text for special return variable in symbol table

--- src/main/java/edu/byu/cs329/typechecker/SymbolTable.java
+++ src/main/java/edu/byu/cs329/typechecker/SymbolTable.java
@@ -15,9 +15,10 @@ public interface SymbolTable {
    * @ensures name \in BOOL ==> type = BOOL
    * @ensures name \in TypeMap ==> type = TypeMap(name)
    * @ensures else ==> type = ERROR
-   * 
+   *
    * @param name is a local variable or parameter as in "v",
-   *     an object instance reference as "this", 
+   *     an object instance reference as "this",
+   *     a variable as "return" to easily get the method type,
    *     a field reference as "class"."field" as in "A.f",
    *     a method reference as "class"."method" as in "A.m",
    *     a boolean literal as in "true" or "false", or
@@ -33,12 +34,12 @@ public interface SymbolTable {
    * @return the type list for associated parameter names
    */
   public List<SimpleImmutableEntry<String, String>> getParameterTypeList(String name);
-  
+
   /**
    * Pushes new scope for local variables.
    */
   public void pushScope();
-  
+
   /**
    * Pops the current local variable scope.
    */
@@ -48,9 +49,9 @@ public interface SymbolTable {
    * Associates the type with the name in the top scope on the stack.
    *
    * @requires name \not\in Types
-   * 
+   *
    * @ensures name \in Types /\ Types(name) = type
-   * 
+   *
    * @param name a local variable name
    * @param type the type of the local variable
    */
